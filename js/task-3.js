2cece333f41dd0c6894a688a8bc6e4124095599e
oksanadmitriieva <145439444+oksanadmitriieva@users.noreply.github.com>
2024-02-23 08:03:09 +0100

    js
    js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,22 @@
 'use strict';
-function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
-}
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
